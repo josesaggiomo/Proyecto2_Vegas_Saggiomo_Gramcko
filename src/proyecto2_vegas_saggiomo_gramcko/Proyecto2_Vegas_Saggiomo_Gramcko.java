3d5587e8286406204c9674bb914d74c4d999473b
Jose Saggiomo <jrsaggiomo@hotmail.com>
2024-11-09 12:45:05 -0400

    Se agrega el proyecto

---
+++ src/proyecto2_vegas_saggiomo_gramcko/Proyecto2_Vegas_Saggiomo_Gramcko.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2_vegas_saggiomo_gramcko;
+
+/**
+ *
+ * @author jrsag
+ */
+public class Proyecto2_Vegas_Saggiomo_Gramcko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
